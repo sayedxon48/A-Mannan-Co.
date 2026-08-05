080b64b39d978a6177c7b0b828c96929873b1456
sayedxon48 <sayedxon48@gmail.com>
2026-08-06 02:05:06 +0600

    Remove sayed@amannan.cc from contact emails

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -36,7 +36,6 @@ const CONTACT_INFO: {
     label: "Email",
     lines: [
       { text: "support@amannan.cc", href: "mailto:support@amannan.cc" },
-      { text: "sayed@amannan.cc", href: "mailto:sayed@amannan.cc" },
       { text: "mannan_fca@yahoo.com", href: "mailto:mannan_fca@yahoo.com" },
       { text: "amannanfca@gmail.com", href: "mailto:amannanfca@gmail.com" },
     ],
